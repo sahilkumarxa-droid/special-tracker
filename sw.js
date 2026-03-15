5c8d7c6cc49427f1af59f67414af07a9f09eeea8
sahilkumarxa-droid <sahilkumarxa@gmail.com>
2026-03-15 22:19:08 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+﻿const CACHE_NAME = "special-tracker-v1";
+const ASSETS = [
+  "./trecker.html",
+  "./manifest.json",
+  "./sw.js",
+  "./1.jpg",
+  "./2.jpg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      }).catch(() => cached)
+    )
+  );
+});
+
